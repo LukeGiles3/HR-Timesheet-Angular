41fef819cd7fc33f45157ac7ffebbd7915fb70be
LukeGiles3 <luke.giles333@gmail.com>
2020-10-02 10:12:00 -0600

    unit 12 async pipes

--- src/app/components/departments/departments.component.ts
+++ src/app/components/departments/departments.component.ts
@@ -1,3 +1,4 @@
+import { Observable } from 'rxjs';
 import { DepartmentsService } from './../../services/departments.service';
 import { Component, OnInit } from '@angular/core';
 import { Department } from 'src/app/interfaces/department';
@@ -10,6 +11,7 @@ import { Router } from '@angular/router';
 })
 export class DepartmentsComponent implements OnInit {
   departments: Department[];
+  $departments: Observable<Department[]>;
 
   constructor(
     private departmentsService: DepartmentsService,
@@ -17,9 +19,10 @@ export class DepartmentsComponent implements OnInit {
     ) { }
 
   ngOnInit(): void {
-    this.departmentsService.getDeparments().subscribe(departments => {
-      this.departments = departments;
-  });
+  //   this.departmentsService.getDeparments().subscribe(departments => {
+  //     this.departments = departments;
+  // });
+  this.$departments = this.departmentsService.getDeparments();
   }
 
   goToDepartment(departmentId: string): void {
